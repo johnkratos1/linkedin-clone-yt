a9b155dc322d75822e38b2c5defef4a8c20c4c7c
johnkratos1 <113154336+johnkratos1@users.noreply.github.com>
2023-04-22 18:24:30 +0100

    install create vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='App'> 
+        Welcome to the React Redux Application
+      </div>
+       
+    
+  )
+}
+
+export default App
